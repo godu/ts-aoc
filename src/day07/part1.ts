a1b82dd446a01dc69ec541b5f92bae7c1055b3eb
godu <arthur.weber@viacesi.fr>
2022-12-07 10:40:10 +0100

    WIP

--- src/day07/part1.ts
+++ src/day07/part1.ts
@@ -1,6 +1,69 @@
-import {identity} from 'fp-ts/lib/function';
+import {constant, flow, identity, pipe} from 'fp-ts/lib/function';
+import {stringify} from 'fp-ts/lib/Json';
+import * as E from 'fp-ts/lib/Either';
+import * as T from 'fp-ts/lib/Tuple';
+import {parser, string} from 'parser-ts';
 import {type Solver} from '../type';
+import {add, endOfFile, endOfLine, parse, space, trace} from '../util';
 
-const solver: Solver = identity;
+type Path = string;
+type Size = number;
+type File = ['file', Path, Size];
+type Dir = ['dir', Path];
+
+type Cd = ['cd', Path];
+type Ls = ['ls', Array<File | Dir>];
+
+type Command = Cd | Ls;
+
+const cdParser: parser.Parser<string, Cd> = pipe(
+	string.string('cd '),
+	parser.apSecond(string.notSpaces1),
+	parser.map((path) => ['cd', path]),
+);
+
+const fileParser: parser.Parser<string, File> = pipe(
+	string.int,
+	parser.apFirst(space),
+	add(string.notSpaces1),
+	parser.map(([size, path]) => ['file', path, size]),
+);
+
+const dirParser: parser.Parser<string, Dir> = pipe(
+	string.string('dir '),
+	parser.apSecond(string.notSpaces1),
+	parser.map((path) => ['dir', path]),
+);
+
+const lsParser: parser.Parser<string, Ls> = pipe(
+	string.string('ls'),
+	parser.apFirst(endOfLine),
+	parser.apSecond(
+		parser.sepBy(
+			endOfLine,
+			pipe(fileParser, parser.alt<string, Dir | File>(constant(dirParser))),
+		),
+	),
+	parser.map((files) => ['ls', files]),
+);
+
+const commandParser: parser.Parser<string, Command> = pipe(
+	cdParser,
+	parser.alt<string, Command>(constant(lsParser)),
+);
+
+const inputParser: parser.Parser<string, Command[]> = pipe(
+	parser.sepBy1(
+		endOfLine,
+		pipe(string.string('$ '), parser.apSecond(commandParser)),
+	),
+	parser.apFirst(endOfFile),
+);
+
+const solver: Solver = flow(
+	parse(inputParser),
+	E.bimap(trace('error\n'), trace('input\n')),
+	E.fold(constant(''), constant('')),
+);
 
 export default solver;
--- src/day07/test.ts
+++ src/day07/test.ts
@@ -3,5 +3,63 @@ import {resolveInput} from '../util/test/helper';
 import part1 from './part1';
 import part2 from './part2';
 
-test('part1', resolveInput, part1, 'part1', 'part1');
-test('part2', resolveInput, part2, 'part2', 'part2');
+test(
+	'part1',
+	resolveInput,
+	part1,
+	`$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k
+`,
+	'95437',
+);
+test(
+	'part2',
+	resolveInput,
+	part2,
+	`$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k
+`,
+	'part2',
+);
